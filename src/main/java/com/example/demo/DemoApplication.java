953d1881fd9bc6fad1b04b40218b07f392b7950c
wesley0983 <wesley0983@yahoo.com.tw>
2019-08-05 23:49:09 +0800

    spring admin-client 設定

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
+//@EnableAdminServer
 public class DemoApplication {
 
     public static void main(String[] args) {
